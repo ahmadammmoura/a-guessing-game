c662a121973c020f69070705766d7aa1c2717788
ahmadammmoura <ammoura575@gmail.com>
2021-03-16 21:04:05 +0200

    done score

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 'use strict'
 
-let name = prompt('what is your name')
+let score = []
 
-document.getElementById('welcome').innerHTML += '<h1>welcome ' + name + ' i am ahmad ammoura welcome to my quiz</h1>'
+let Name = prompt('what is your name')
+
+document.getElementById('welcome').innerHTML += '<h1>welcome '+ Name +' i am ahmad ammoura welcome to my quiz</h1>'
 
 let Q1 = prompt('Am I 25 years old ?').toLocaleLowerCase()
 if (Q1 === 'yes' || Q1 === 'y') {
     document.getElementById('answer1').innerHTML += `<h5 style="color: green;" >True</h5>`;
-    alert('true')
+    alert('true');
+    score.push(1)
 } else {
     document.getElementById('answer1').innerHTML += `<h5 style="color: red;" >wrong</h5>`;
     alert('bad start')
@@ -16,7 +19,9 @@ if (Q1 === 'yes' || Q1 === 'y') {
 let Q2 = prompt('graduated from Albalqaa University ?').toLocaleLowerCase()
 if (Q2 === 'yes' || Q2 === 'y') {
     document.getElementById('answer2').innerHTML += `<h5 style="color: green;" >True</h5>`;
-    alert('true')
+    alert('true');
+    score.push(1)
+
 
 
 } else {
@@ -28,7 +33,9 @@ let Q3 = prompt('my fav football team real madried ?').toLocaleLowerCase()
 
 if (Q3 === 'no' || Q3 === 'n') {
     document.getElementById('answer3').innerHTML += `<h5 style="color: green;" >True</h5>`;
-    alert('ANYTHING')
+    alert('ANYTHING');
+    score.push(1)
+
 
 } else {
     document.getElementById('answer3').innerHTML += `<h5 style="color: red;" >wrong</h5>`;
@@ -39,6 +46,8 @@ let Q4 = prompt('watch basketball alot ?').toLocaleLowerCase()
 if (Q4 === 'yes' || Q4 === 'y') {
     document.getElementById('answer4').innerHTML += `<h5 style="color: green;" >True</h5>`;
     alert('HAHAHA')
+    score.push(1)
+
 } else {
     document.getElementById('answer4').innerHTML += `<h5 style="color: red;" >wrong</h5>`
     alert('wrong')
@@ -50,6 +59,8 @@ let Q5 = prompt('i dont like watching movies?').toLocaleLowerCase()
 if (Q5 === 'yes' || Q5 === 'y') {
     document.getElementById('answer5').innerHTML += `<h5 style="color: green;" >True</h5>`;
     alert('true')
+    score.push(1)
+
 } else {
     document.getElementById('answer5').innerHTML += `<h5 style="color: red;" >wrong</h5>`;
     alert('wrong')
@@ -67,7 +78,8 @@ for (let i = 5; i > 0; i--) {
 
     let Q6 = prompt('what the number that i am thinking about between 0 to 10 you have ' + attempt + ' attempts')
     if (Q6 == 7) {
-        alert('right answer')
+        alert('right answer');
+        score.push(1)
         break;
     } else if (Q6 > 7) {
         alert('too high')
@@ -95,7 +107,8 @@ for(let i = 6 ; i >= 1 ; i-- ){
 
         if(Q7 == pleyers[j]){
             alert('thats right');
-            correct = true
+            correct = true;
+            score.push(1)
             break ;
         }
 
@@ -110,7 +123,11 @@ for(let i = 6 ; i >= 1 ; i-- ){
     if(gusses==0){
         alert('poor you')
     }
-
-
-
 }
+
+let sum = 0
+for(let i = 0 ; i < score.length ; i++){
+    sum+=score[i]
+}
+
+alert('you got '+sum +'/7 Good job '+Name )
